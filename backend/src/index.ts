28521a7153698b1e3a0fbcf002a25c907ce364e1
lebrancconvas <lebrancconvas@gmail.com>
2025-08-30 18:11:29 +0700

    🔧 chore(backend): update environment variable (PORT) in .env file.

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,8 +1,11 @@
 import express, { type Request, type Response } from "express";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const app = express();
 
-const PORT = 8000;
+const PORT = process.env.PORT || 8000;
 
 app.get("/", (req: Request, res: Response) => {
   res.send("PDPA Management System API starts working!");
