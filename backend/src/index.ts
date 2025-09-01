6c81389bc874fe27d7183bb2cdf1219a3bcea900
lebrancconvas <lebrancconvas@gmail.com>
2025-09-01 14:58:15 +0700

    ✨ feat(backend): manage middleware for request body and logging api status.

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,12 +1,16 @@
 import express from "express";
 import router from "./router/index.js";
 import dotenv from "dotenv";
+import morgan from "morgan";
 
 dotenv.config();
 
 const app = express();
 
 const PORT = process.env.PORT || 8000;
+
+app.use(express.json());
+app.use(morgan('dev'));
 
 app.use("/api", router);
 
