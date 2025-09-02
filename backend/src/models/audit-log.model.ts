6a383361d453138747de190e80feae73b44598d2
lebrancconvas <lebrancconvas@gmail.com>
2025-09-02 12:13:46 +0700

    ✨ feat(backend): create audit logs model functions for the back-end api.

---
+++ backend/src/models/audit-log.model.ts
@@ -0,0 +1,9 @@
+// - (POST) `/audit_logs`
+export const createAuditLogModel = (req: Request, res: Response) => {
+
+};
+
+// - (GET) `/audit_logs`
+export const readAuditLogsModel = (req: Request, res: Response) => {
+  
+};
