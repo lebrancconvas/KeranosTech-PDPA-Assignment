b66a99f066a5a0052884654597ee7b32e776c3ac
lebrancconvas <lebrancconvas@gmail.com>
2025-09-02 12:13:17 +0700

    ✨ feat(backend): create data requests model functions for the back-end api.

---
+++ backend/src/models/data-request.model.ts
@@ -0,0 +1,19 @@
+// - (POST) `/data_requests`
+export const createDataRequestModel = (req: Request, res: Response) => {
+  
+};
+
+// - (GET) `/data_requests`
+export const readDataRequestModel = (req: Request, res: Response) => {
+
+};
+
+// - (GET) `/data_requests/<request_id>`
+export const readDataRequestByIdModel = (req: Request, res: Response) => {
+
+};
+
+// - (PUT) `/data_requests/<request_id>/status`
+export const updateDataRequestStatusByIdModel = (req: Request, res: Response) => {
+
+};
