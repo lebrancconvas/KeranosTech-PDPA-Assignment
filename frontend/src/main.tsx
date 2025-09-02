833447a2c34eb0954fb1e638481638102dcbe084
lebrancconvas <lebrancconvas@gmail.com>
2025-09-03 02:42:02 +0700

    ✨ feat(frontend): update front-end router via react-router-dom.

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,25 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import './index.css'
+
+// Import Page Components.
 import App from './App.tsx'
+import NotFound from './pages/errors/NotFound.tsx';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    errorElement: <NotFound />,
+    children: [
+
+    ]
+  }
+]);
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
-)
+);
