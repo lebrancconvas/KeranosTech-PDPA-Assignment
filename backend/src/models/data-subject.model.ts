b4ce8d5a8171042eef115a31cc3c78bae18b6b84
lebrancconvas <lebrancconvas@gmail.com>
2025-09-02 11:58:30 +0700

    ✨ feat(backend): create data subjects model function for the back-end api.

---
+++ backend/src/models/data-subject.model.ts
@@ -0,0 +1,29 @@
+// - (POST) `/data_subjects`
+export const createDataSubjectModel = (req: Request, res: Response) => {
+  
+};
+
+// - (GET) `/data_subjects`
+export const readDataSubjectModel = (req: Request, res: Response) => {
+
+};
+
+// - (GET) `/data_subjects/<data_subject_id>`
+export const readDataSubjectByIdModel = (req: Request, res: Response) => {
+
+};
+
+// - (PUT) `/data_subjects/<data_subject_id>`
+export const updateDataSubjectByIdModel = (req: Request, res: Response) => {
+  
+};
+
+// - (GET) `/data_subjects/<data_subject_id>/consents`
+export const readDataSubjectConsentByIdModel = (req: Request, res: Response) => {
+
+};
+
+// - (PUT) `/data_subjects/<data_subject_id>/consents`
+export const updateDataSubjectConsentActiveByIdModel = (req: Request, res: Response) => {
+
+};
