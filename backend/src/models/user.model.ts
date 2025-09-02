8e9db31238e654f919dc20db7e99699064887f66
lebrancconvas <lebrancconvas@gmail.com>
2025-09-02 12:13:33 +0700

    ✨ feat(backend): create users model functions for the back-end api.

---
+++ backend/src/models/user.model.ts
@@ -0,0 +1,27 @@
+// - **Authentication**
+
+//   - (POST) `/auth/register`
+export const registerNewUserModel = (req: Request, res: Response) => {
+
+};
+
+
+//   - (POST) `/auth/login`
+export const loginUserModel = (req: Request, res: Response) => {
+
+};
+
+/* ********************************************************* */
+
+// - **Users**
+
+//   - (GET) `/users`
+export const readUsersModel = (req: Request, res: Response) => {
+
+};
+
+//   - (GET) `/me`
+export const readLoginUserModel = (req: Request, res: Response) => {
+  
+};
+
