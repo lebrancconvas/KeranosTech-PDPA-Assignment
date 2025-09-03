1d2e86a891849bbd57fbcceff327d98d42b75292
lebrancconvas <lebrancconvas@gmail.com>
2025-09-03 07:31:36 +0700

    ✨ feat(frontend): create footer for the front-end.

---
+++ frontend/src/components/Footer.tsx
@@ -0,0 +1,8 @@
+function Footer() {
+  <div>
+    Made by <a href="https://github.com/lebrancconvas" target="_blank">Poom Yimyuean (lebrancconvas)</a>
+    <a href="https://www.flaticon.com/free-icons/privacy" title="privacy icons">Privacy icons created by Freepik - Flaticon</a>
+  </div>
+};
+
+export default Footer;
